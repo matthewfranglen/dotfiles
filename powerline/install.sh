faa5476ce9adbadaec602972c1df63a5f25419ac
Matthew Franglen <matthew@franglen.org>
2015-03-13 11:08:09 +0000

    Try to use both wget and curl for installing pip

--- powerline/install.sh
+++ powerline/install.sh
@@ -1,8 +1,26 @@
+function get_url_to_file () {
+    local url=$1
+    local filename=$2
+
+    if which wget >/dev/null
+    then
+        wget --output-document "${filename}" "${url}"
+    elif which curl >/dev/null
+    then
+        curl --output "${filename}" "${url}"
+    else
+        echo "Unable to install powerline... wget and curl commands not found"
+        exit 0
+    fi
+}
+
+GET_PIP_FILE=/tmp/get-pip.py
+
 if [ ! -e ~/.local/bin/pip ]
 then
-    wget --output-document /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-    python /tmp/get-pip.py --user
-    rm /tmp/get-pip.py
+    get_url_to_file "https://bootstrap.pypa.io/get-pip.py" "${GET_PIP_FILE}"
+    python "${GET_PIP_FILE}" --user
+    rm "${GET_PIP_FILE}"
 fi
 
 ~/.local/bin/pip install --user powerline-status
