4151c683abaca2081d35b8f6f893213913a79a06
Matthew Franglen <matthew@franglen.org>
2015-03-13 11:10:41 +0000

    Clean up the powerline install script
    It is in plain 'sh' so the function syntax is different. Also might as
    well make it proper like.

--- powerline/install.sh
+++ powerline/install.sh
@@ -1,4 +1,24 @@
-function get_url_to_file () {
+
+install () {
+    install_pip && install_powerline
+}
+
+install_powerline () {
+    ~/.local/bin/pip install --user powerline-status
+}
+
+install_pip () {
+    local get_pip_file=/tmp/get-pip.py
+
+    if [ ! -e ~/.local/bin/pip ]
+    then
+        get_url_to_file "https://bootstrap.pypa.io/get-pip.py" "${get_pip_file}" || return 1
+        python "${get_pip_file}" --user
+        rm "${get_pip_file}"
+    fi
+}
+
+get_url_to_file () {
     local url=$1
     local filename=$2
 
@@ -10,17 +30,8 @@ function get_url_to_file () {
         curl --output "${filename}" "${url}"
     else
         echo "Unable to install powerline... wget and curl commands not found"
-        exit 0
+        return 1
     fi
 }
 
-GET_PIP_FILE=/tmp/get-pip.py
-
-if [ ! -e ~/.local/bin/pip ]
-then
-    get_url_to_file "https://bootstrap.pypa.io/get-pip.py" "${GET_PIP_FILE}"
-    python "${GET_PIP_FILE}" --user
-    rm "${GET_PIP_FILE}"
-fi
-
-~/.local/bin/pip install --user powerline-status
+install
