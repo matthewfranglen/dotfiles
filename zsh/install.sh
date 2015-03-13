dbcbb6b3fa5480aaaa0ea88e210af15688425b89
Matthew Franglen <matthew@franglen.org>
2015-03-13 11:14:34 +0000

    Update zsh installer to use curl/wget and clean

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,11 +1,34 @@
-if [ ! -e ~/.antigen.zsh ]
-then
-    wget --output-document ~/.antigen.zsh https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh
-fi
 
-if [ ! -e ~/.fasd.zsh ]
-then
-    wget --output-document ~/.fasd.zsh https://raw.githubusercontent.com/clvv/fasd/master/fasd
-    chmod 755 ~/.fasd.zsh
-    ln -s ~/.fasd.zsh ~/.local/bin/fasd
-fi
+install () {
+    install_antigen
+    install_fasd
+}
+
+install_antigen () {
+    get_url_to_file "https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh" "~/.antigen.zsh" || return 1
+}
+
+install_fasd () {
+    local fasd_script_file="~/.fasd.zsh"
+    get_url_to_file "https://raw.githubusercontent.com/clvv/fasd/master/fasd" "${fasd_script_file}" || return 1
+    chmod 755 "${fasd_script_file}"
+    ln -s "${fasd_script_file}" ~/.local/bin/fasd
+}
+
+get_url_to_file () {
+    local url=$1
+    local filename=$2
+
+    if which wget >/dev/null
+    then
+        wget --output-document "${filename}" "${url}"
+    elif which curl >/dev/null
+    then
+        curl --output "${filename}" "${url}"
+    else
+        echo "Unable to install zsh... wget and curl commands not found"
+        return 1
+    fi
+}
+
+install
