88058968e2274dd9ea699e612d98bc472fcc2d56
Matthew Franglen <matthew@franglen.org>
2015-03-19 22:59:29 +0000

    Ditch the python stuff
    The local folder is supposed to be sourced automatically. My problems
    were related to other issues.

--- python/path.zsh
+++
@@ -1,8 +0,0 @@
-if [ -z ${PYTHONPATH} ]
-then
-    PYTHONPATH=~/.local/lib/python2.7/site-packages/
-else
-    PYTHONPATH=${PYTHONPATH}:~/.local/lib/python2.7/site-packages/
-fi
-
-export PYTHONPATH
