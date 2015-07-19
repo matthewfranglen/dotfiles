4d9f9b0184ad91dcade1de7d167facf50fdab290
Matthew Franglen <matthew@franglen.org>
2015-07-19 15:55:15 +0100

    Disable global menu
    It's just awful.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -47,4 +47,8 @@ fi
 
 export LC_ALL="en_GB.utf8"
 
+# https://wiki.ubuntu.com/DesktopExperienceTeam/ApplicationMenu
+# Disables global menu for everything.
+export UBUNTU_MENUPROXY=
+
 # vim: set ai et sw=4 syntax=zsh :
