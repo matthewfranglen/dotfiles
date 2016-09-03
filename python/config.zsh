b4666384712be9535afa9ed4fe6416168e21e7ec
Matthew Franglen <matthew@franglen.org>
2016-09-03 02:19:00 +0100

    Load pyenv if present

--- python/config.zsh
+++ python/config.zsh
@@ -2,3 +2,10 @@ if [ -e ~/.local/bin/virtualenvwrapper_lazy.sh ]
 then
     source ~/.local/bin/virtualenvwrapper_lazy.sh
 fi
+
+if [ -e ~/.pyenv/bin ]
+then
+    export PATH="/home/matthew/.pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
