0a30c2b2689f684126814db83b973c1e3efcd393
Matthew Franglen <matthew@franglen.org>
2016-09-17 22:20:48 +0100

    Fonts writes to FD 3 to indicate skipping

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,17 +3,17 @@ set -eu
 . "`dirname \`dirname \\\`readlink -f $0\\\`\``/script/lib.sh"
 
 install () {
-    ( validate && install_fonts ) || true
+    validate && install_fonts
 }
 
 validate () {
     if ! is_on_local_machine
     then
-        echo -- "Not installing fonts... currently running on remote host" >&2
+        echo -- "Not installing fonts... currently running on remote host" >&3
         return 1
     elif ! which fc-cache >/dev/null
     then
-        echo "Unable to install fonts... fc-cache command not found"
+        echo -- "Unable to install fonts... fc-cache command not found" >&2
         return 1
     fi
     return 0
