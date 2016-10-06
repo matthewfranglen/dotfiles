73a65ce9e04324c7533e7aa7533d14ef50a134eb
Matthew Franglen <matthew@franglen.org>
2016-10-06 19:52:27 +0100

    Quote url

--- zsh/install.sh
+++ zsh/install.sh
@@ -36,7 +36,7 @@ install_fzf () {
         return
     fi
 
-    git clone --quiet --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+    git clone --quiet --depth 1 "https://github.com/junegunn/fzf.git" "${HOME}/.fzf"
     "${HOME}/.fzf/install" --no-completion --key-bindings --update-rc
 }
 
