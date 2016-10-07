48569bee6a7a07b68572a90e7e900df3502fc952
Matthew Franglen <matthew@franglen.org>
2016-10-07 22:33:03 +0100

    Add delimiter before command time

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -175,7 +175,7 @@ command_execution_status() {
     then
         return
     fi
-    print -P "%K{${COMMAND_TIME_BACKGROUND_COLOR}}%F{${COMMAND_TIME_FOREGROUND_COLOR}}${execution_time}%F{${COMMAND_TIME_BACKGROUND_COLOR}}"
+    print -P "%K{${COMMAND_TIME_BACKGROUND_COLOR}} %F{${COMMAND_TIME_FOREGROUND_COLOR}}${execution_time}%F{${COMMAND_TIME_BACKGROUND_COLOR}}"
 }
 
 docker_machine_status() {
