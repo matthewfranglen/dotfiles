1d3a777a8697a2a699c3e520ba419867d59aa84d
Matthew Franglen <matthew@franglen.org>
2017-05-02 21:05:32 +0100

    Start using oh-my-zsh
    I'm finally doing it.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,4 @@ alias lla='ls -al'
 
 alias youtube='each "youtube-dl --extract-audio --output \"%(title)s.%(ext)s\" \"\$1\""'
 
-alias awkf="awk '{ print \$1 }'"
-alias awkl="awk '{ print \$NF }'"
-
 # vim: set ai et sw=4 syntax=zsh :
