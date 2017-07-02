ada85f982a8e96b2ae9d083d8e8730d6bf9993b2
Matthew Franglen <matthew@franglen.org>
2017-07-02 19:32:01 +0100

    Stop initializing pyenv twice

--- python/config.zsh
+++ python/config.zsh
@@ -3,8 +3,9 @@ then
     source ~/.local/bin/virtualenvwrapper_lazy.sh
 fi
 
-if [ -e ~/.pyenv/bin ]
+if [ -e ~/.pyenv/bin ] && [ -z "${PYENV_INIT}" ]
 then
+    export PYENV_INIT=1
     export PATH="${HOME}/.pyenv/bin:$PATH"
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
