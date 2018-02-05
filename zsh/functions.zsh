f05b5e029cba6131feae4f5c4ceeec8783f8bf1f
Matthew Franglen <matthew@franglen.org>
2018-02-05 19:13:59 +0000

    Update docker_clean command
    Should remove it when I get used to this

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -130,9 +130,7 @@ function zip_recursive_search () {
 }
 
 function docker_clean () {
-    docker rm $(docker ps -aq 2>/dev/null) 2> /dev/null
-    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2> /dev/null
-    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2> /dev/null
+    docker system prune -a
 }
 
 function mgk () {
